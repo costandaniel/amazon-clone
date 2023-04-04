69c76c8b56fcc3a083c68e5d8bacb13a4d64aff9
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-04-04 21:20:16 +0300

    initialization

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB5tuDpF6_zhAm3g1GN5c788xgJRWkiyPY',
+  authDomain: 'practice-a938b.firebaseapp.com',
+  projectId: 'practice-a938b',
+  storageBucket: 'practice-a938b.appspot.com',
+  messagingSenderId: '544604428468',
+  appId: '1:544604428468:web:4eb2d509fa5ed707af5fe1',
+};
