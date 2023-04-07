c40f7cef9e0000236fc93eb69cecff9390ba4027
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-04-07 14:32:10 +0300

    finished homepage

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import '../CSS/Product.css';
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The Lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>🌟</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-        alt="img"
-      />
+      <img src={image} alt="img" />
       <button>Add to Basket</button>
     </div>
   );
