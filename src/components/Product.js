4aa72f88e75ca528a853075ccf8ddfb344084689
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-04-07 14:07:04 +0300

    working Home page

---
+++ src/components/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../CSS/Product.css';
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The Lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>🌟</p>
+        </div>
+      </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt="img"
+      />
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
