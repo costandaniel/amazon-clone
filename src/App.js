bcc3c2d461c900b5eebb2f9e0dde69158373e6fa
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-04-14 13:26:07 +0300

    completed signIn design

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Checkout from './components/Checkout';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 function App() {
   return (
-      // BEM
-      <Router>
+    // BEM
+    <Router>
       <div className="app">
         <Routes>
-          <Route path="/checkout" element={[<Header />,<Checkout />]}/>
-          <Route path="/" element={[<Header />, <Home />]}/>
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route path="/" element={[<Header />, <Home />]} />
+          <Route path="/login" element={[<Login />]} />
         </Routes>
       </div>
     </Router>
